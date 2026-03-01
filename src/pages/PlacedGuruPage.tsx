f27e634f1290f7ce8e70fabe39a6ee6b227a7a87
Nallana Hari Krishna <322107311061@andhrauniversity.edu.in>
2026-03-01 10:02:28 +0530

    added clientid adn redirect url

--- src/pages/PlacedGuruPage.tsx
+++ src/pages/PlacedGuruPage.tsx
@@ -14,7 +14,9 @@ import { Expert } from './connect/ExpertCard';
 function startGoogleOAuth(expertId: string) {
   const clientId    = import.meta.env.VITE_GOOGLE_CLIENT_ID;
   const redirectUri = `${window.location.origin}/oauth/google/callback`;
-  alert(`redirect_uri: ${redirectUri}\n\nclient_id: ${clientId}`);
+  const scope       = 'https://www.googleapis.com/auth/meetings.space.created';
+  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}&access_type=offline&prompt=consent&state=${expertId}`;
+  window.location.href = url;
 }
 
 const MyExpertCard = ({
